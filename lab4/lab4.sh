ad007bd77a588fb4418d4b31a5399abd55ea1ba3
Andrei <andrei.popescu5@student.upt.ro>
2025-03-19 09:34:59 +0200

    problema lab4

--- lab4/lab4.sh
+++ lab4/lab4.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/bash
 
+while read x
+do
+    echo $x | grep -E "[A-Za-z0-9]*"  | grep -E "[ ]*" | grep -E "[,]*" | grep -E "^[A-Z]" | grep -E "(\.)$" | grep -E -v "(\.)\1{1}$" | grep -E -v "si[ ]*," | grep -E -v "n[pb]"
+done
